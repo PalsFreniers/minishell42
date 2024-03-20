e1ded3b308bab0ba08f547e303e12941e3e55a99
dosokin <dosokin@oneonmail.com>
2024-03-20 21:58:52 +0100

    path fix

--- src/paths_related.c
+++ src/paths_related.c
@@ -6,7 +6,7 @@
 /*   By: dosokin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:06:25 by dosokin           #+#    #+#             */
-/*   Updated: 2024/03/05 23:30:01 by tdelage          ###   ########.fr       */
+/*   Updated: 2024/03/20 21:58:13 by dosokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,15 @@ char	**get_splitted_path(char **envp)
 		return (NULL);
 	splitted_paths = ft_split(paths, ':');
 	free(paths);
+	if (!splitted_paths[0])
+	{
+		free(splitted_paths[0]);
+		free(splitted_paths);
+		splitted_paths = malloc(2 * sizeof(char *));
+		splitted_paths[0] = malloc(2 * sizeof(char));
+		splitted_paths[0][0] = '.';
+		splitted_paths[0][1] = 0;
+		splitted_paths[1] = NULL;
+	}
 	return (splitted_paths);
 }
